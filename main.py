bc455eb2efbabb4813cfd54d545536c0353a33ff
Samuel Terra <samuelterra22@hotmail.com>
2019-11-21 18:20:21 -0300

    Implement read function

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('Hello')
+def le_arquivo(arquivo):
+    # Abre arquivo para leitura
+    f = open(arquivo, "r")
+
+    # Pega as linhas
+    linhas = f.readlines()
+
+    # Pega as duas primeiras linhas e joga numa lista
+    cabecalho = [int(linhas[i].replace("\n", "")) for i in range(2)]
+
+    # Lê as n linhas restantantes transformando os valores em inteiros e depois como tuplas de arcos (u, v, w)
+    arcos = [tuple(map(int, linhas[i].replace("\n", "").split())) for i in range(2, len(linhas))]
+
+    # Retorna o cabeçalho e a lista de arcos (u, v, w)
+    return cabecalho, arcos
+
+
+def calcula_funcao_objetivo():
+    pass
+
+
+if __name__ == '__main__':
+    # Lê o arquivo de teste
+    cab, arcs = le_arquivo('./testes/DAGs/100_01_025_0.dag')
