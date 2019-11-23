60fd2622d836ce09edeac03f854008376fd92a5c
Wesley Henrique Batista Nunes <wesley.rn2@gmail.com>
2019-11-23 20:50:37 -0300

    Fix bugs

--- main.py
+++ main.py
@@ -37,29 +37,35 @@ def resolve(no_inicial, lista_de_arcos, n):
         for e in lista_de_arcos:
             if e[0] in ultimos_adicionados:
                 aux.append(e)
-
         aux.sort(key=lambda tup: tup[2], reverse=False)
         ultimos_adicionados = []
+        arestas_para_remover = []
         melhor_aresta_positiva = None
         for e in aux:
-            if not tem_ciclo(e, V, lista_de_arcos):
+            if not tem_ciclo(e, V, solucao_final):
                 if e[2] <= 0:
                     solucao_final.append(e)
-                    V.append(e[1])
-                    aux.remove(e)
-                    ultimos_adicionados.append(e[1])
+                    arestas_para_remover.append(e)
+                    print(len(V), len(aux))
+                    if e[1] not in V:
+                        V.append(e[1])
+                        ultimos_adicionados.append(e[1])
                 else:
                     melhor_aresta_positiva = e
                     break
-
+        for aresta in arestas_para_remover:
+            aux.remove(aresta)
         if not ultimos_adicionados:
             delta = (n - len(V)) / n
             r = random.uniform(0, 1)
-            if r < delta:
+            if r < delta and melhor_aresta_positiva is not None:
+                if melhor_aresta_positiva not in solucao_final:
+                    solucao_final.append(melhor_aresta_positiva)
                 solucao_final.append(melhor_aresta_positiva)
-                V.append(melhor_aresta_positiva[1])
+                if melhor_aresta_positiva[1] not in V:
+                    V.append(melhor_aresta_positiva[1])
+                    ultimos_adicionados.append(melhor_aresta_positiva[1])
                 aux.remove(melhor_aresta_positiva)
-                ultimos_adicionados.append(melhor_aresta_positiva[1])
             else:
                 stop = True
     return solucao_final
@@ -71,7 +77,7 @@ def calcula_funcao_objetivo(solucao):
 
 if __name__ == '__main__':
     # Lê o arquivo de teste
-    cab, arcs = le_arquivo('./testes/DAGs/100_01_050_0.dag')
+    cab, arcs = le_arquivo('./testes/DAGs/1000_01_025_0.dag')
     s = resolve(0, arcs, cab[0])
 
     print(s)
