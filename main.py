70f714632a92126ee32bda57b19545441042f05a
Samuel Terra <samuelterra22@hotmail.com>
2019-11-25 13:42:52 -0300

    Add comments

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import random
 
 
 def le_arquivo(arquivo):
+    """
+    Função responsável por realizar a leitura da instância no arquivo.
+    :param arquivo: Caminho do arquivo (instância)
+    :return:        Retorna o cabeçalho contendo as duas primeiras linhas do arquivo
+                    e a lista de arcos.
+    """
+
     # Abre arquivo para leitura
     f = open(arquivo, "r")
 
@@ -20,8 +27,20 @@ def le_arquivo(arquivo):
 
 
 def resolve(no_inicial, lista_de_arcos, n):
+    """
+    Função Responsável por contruir a árvore direciona T a partir da lista de arcos
+     que formam o grafo.
+    :param no_inicial:      Id do vértice inicial do grafo.
+    :param lista_de_arcos:  Lista de arcos vinda da instancia do arquivo.
+    :param n:               Número de arcos contidos no grafo.
+    :return:                Retorna o vetor solução contendo a lista de arcos que
+                            constituem a árvore direcionada T.
+    """
+
+    # Ordena a lista de arcos de forma crescente
     lista_de_arcos.sort(key=lambda tup: tup[2], reverse=False)
 
+    # Variável de controle para parar laço principal
     stop = False
     solucao_final = []
 
@@ -32,6 +51,8 @@ def resolve(no_inicial, lista_de_arcos, n):
         for e in lista_de_arcos:
             if e[0] in ultimos_adicionados:
                 aux.append(e)
+
+        # Ordena novamente a lista de arcos de forma crescente
         aux.sort(key=lambda tup: tup[2], reverse=False)
         ultimos_adicionados = []
         arestas_para_remover = []
@@ -58,60 +79,117 @@ def resolve(no_inicial, lista_de_arcos, n):
             lista_de_arcos.remove(aresta)
         if not ultimos_adicionados:
             stop = True
+
+    # Retorna o vetor solução com os arcos da árvore
     return solucao_final
 
 
 def calcula_funcao_objetivo(solucao):
+    """
+    Função reponsável por realizar o cálculo da função objetivo.
+    :param solucao: Solução como uma lista de arcos.
+    :return:        Retorna o somatório de todos os pesos dos arcos da árvore.
+    """
     return sum(x[2] for x in solucao)
 
 
 def tem_ciclo_grafo_V2(arco_candidato, V):
+    """
+    Função versão mais simplificada para verificação de ciclo.
+    :param arco_candidato:  Arco candidato (u, v, w) a ser verificado.
+    :param V:               Lista de vértices.
+    :return:                Retorna True se o vértice de destino do arco candidato
+                            estiver contido na lista de vértices.
+    """
+
+    # Verifica se o vértice de destino está contino na lista de vértices.
     if arco_candidato[1] in V:
         return True
     return False
 
 
 def tem_ciclo(arco_candidato, V, solucao):
+    """
+    Função para verificar se há ciclo no grafo ao adiciona um novo vértice no grafo.
+    :param arco_candidato:  Arco candidato (u, v, w) a ser verificado.
+    :param V:               Lista de vértices.
+    :param solucao:         Lista de arcos da solução atual.
+    :return:                Retorna False se for verificado um ciclo com o novo arco
+                            adicionado, caso contrário, retorna True.
+    """
+
+    # Verifica se o vértice de origem do arco candidato nao está contido em V
     if arco_candidato[0] not in V:
         return False
+
+    # Copia o vetor de vértices V para um vetor auxiliar
     aux_V = copy.deepcopy(V)
+
+    # Adiciona o vértice de destino do arco candidato na lista de vértices auxiliar
     aux_V.append(arco_candidato[1])
     arestas_arvore = copy.deepcopy(solucao)
     arestas_arvore.append(arco_candidato)
+
+    # Para todos os vértices
     for v in aux_V:
+        # Retorna VERDADEIRO informando que há ciclo se possui um caminho de
+        # retorno de v para as arestas da sulução
         if tem_caminho_retorno(v, arestas_arvore):
             return True
+    # Se não possui caminho de retorno do vetor de vértices para os vértices da solução,
+    # retorna FALSO informando que não há ciclos
     return False
 
 
 def tem_caminho_retorno(v, solucao):
+    """
+    Função para verificar se possui caminho de retorno de uma lista de
+    arcos para um vertice v.
+    :param v:       Id do vértice.
+    :param solucao: Lista de arcos.
+    :return:        Returna True se tiver um caminho de retorno de algum
+                    arco da lista para o vértice v.
+    """
+    # Lista para controle dos vértices visitados
     vertices_ja_visitados = []
+
+    # Inicia pilha de controle com o vértice informado
     pilha = [v]
+
+    # Enquando a pilha nao estiver vazia
     while pilha:
+        # Retira o vértice do topo da pilha para a análise se há caminho de retorno
         vertice_atual = pilha.pop(0)
+
+        # Para as todas as arestas na lista de solução
         for aresta in solucao:
+            # Verifica se o vertice atual é o mesmo vértice de origem da aresta da
+            # lista de soluções, checando se o vértice atual tem uma aresta pra alguem,
             if vertice_atual == aresta[0]:
+                #  Se tiver aresta e ela não foi visitada, adiciona na pilha
                 if aresta[1] not in vertices_ja_visitados:
                     pilha.append(aresta[1])
+
+                # Se possui uma aresta na solução que volta para onde começou,
+                # retorna VERDADEIRO saindo do método e informando que possue um caminho
+                # de retono
                 if aresta[1] == v:
                     return True
+        # Depois de verificar caminho de retorno para todas as soluções,
+        # adiciona o vértice atual na lista de vertices visitados
         vertices_ja_visitados.append(vertice_atual)
+
+    # Caso não possua caminho de retorno, retorna FALSO
     return False
 
 
 if __name__ == '__main__':
-    # Lê o arquivo de teste
-    cab, arcs = le_arquivo('./testes/DAGs/100_01_050_0.dag')
-    s = resolve(0, arcs, cab[0])
+    # Lê o arquivo da instancia
+    cab, arcos = le_arquivo('./testes/DAGs/100_01_050_0.dag')
 
-    print(s)
-    print(calcula_funcao_objetivo(s))
+    # Constroe a árvore direcionada T a partir do primeiro vértice com os
+    # dados do arquivo de entrada
+    T = resolve(0, arcos, cab[0])
 
-    # V = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # grafo = [
-    #     (0, 1, 0), (0, 2, 0), (0, 7, 0), (4, 5, 0), # (3, 5, 0),
-    #     (2, 4, 0), (5, 6, 0), (7, 6, 0), (7, 8, 0), (8, 6, 0),
-    #     (6, 9, 0), (8, 9, 0), (1, 3, 0), (4, 3, 0), (4, 8, 0),
-    #     (6, 1, 0)
-    # ]
-    # print(tem_ciclo(None, V, grafo))
+    print(T)
+    print(calcula_funcao_objetivo(T))
